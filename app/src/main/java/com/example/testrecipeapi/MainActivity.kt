49c5cc85f08514fb0cec796ed8079d857c9440ba
Elliot Weimann <elliot.weimann@bellevuecollege.edu>
2021-11-17 00:32:41 -0800

    dismiss keyboard on button click

--- app/src/main/java/com/example/testrecipeapi/MainActivity.kt
+++ app/src/main/java/com/example/testrecipeapi/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.testrecipeapi
 
+import android.app.Activity
+import android.content.Context
 import android.os.AsyncTask
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputEditText
 import org.json.JSONArray
 import org.json.JSONObject
@@ -34,8 +39,21 @@ class MainActivity : AppCompatActivity() {
                 search(str)
             }.execute()
 
-
+hideKeyboard()
         }
+    }
+
+    fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
@@ -81,7 +99,6 @@ class MainActivity : AppCompatActivity() {
             }
 
             val txtRes = findViewById<TextView>(R.id.txtResult)
-
 
             this.runOnUiThread(
 
